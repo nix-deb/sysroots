22744901c7213b51ba0048ffec35df88a291de85
Philip Taron <philip.taron@gmail.com>
2026-01-06 15:32:14 -0800

    Restrict flake to x86_64-linux only

--- flake.nix
+++ flake.nix
@@ -3,8 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/x86_64-linux";
     blueprint.url = "github:numtide/blueprint";
     blueprint.inputs.nixpkgs.follows = "nixpkgs";
+    blueprint.inputs.systems.follows = "systems";
   };
 
   outputs = inputs: inputs.blueprint { inherit inputs; };
